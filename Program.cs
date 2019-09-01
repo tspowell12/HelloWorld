c9a21e95af72dd6d7dea76bb6f22edc7eb0c0328
Powell, Tyler <tspowell@tampabay.rr.com>
2019-08-31 22:39:28 -0400

    Changes made to Version 1 of Hello World

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Type any key to continue... Then press 'Enter'");
             Console.WriteLine("Hello World!");
+            // Assigns the user input value to the variable 'Read'
+            string Read = Console.ReadLine();
+            // Displays what the user inputed, along with another message
+            Console.WriteLine("You typed '" + Read+ "'. Have a good day!");
         }
     }
 }
